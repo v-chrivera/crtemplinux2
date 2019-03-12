dc8b77d8f4d5383ee58eb9f4448f5adc4d949337
Juntao Zhu <juzhu@microsoft.com>
2019-03-12 11:30:25 +0800

    Add the missing WarmupController.cs file

---
+++ LabAppInit/Controllers/WarmupController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabAppInit.Controllers
+{
+    public class WarmupController : Controller
+    {
+        static DateTime startupTimestamp = DateTime.Now;
+        // GET: Warmup
+        public string Index()
+        {
+            Thread.Sleep(60000);
+            return "Warmed up";
+        }
+
+        public string NoSleep()
+        {
+            TimeSpan differnce = DateTime.Now - startupTimestamp;
+
+            if (differnce < TimeSpan.FromSeconds(80))
+            {
+                Response.StatusCode = 502;
+            }
+            return "Warming up";
+        }
+    }
+}
